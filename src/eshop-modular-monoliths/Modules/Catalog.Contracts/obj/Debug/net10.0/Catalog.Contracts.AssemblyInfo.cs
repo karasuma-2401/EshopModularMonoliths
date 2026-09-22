3132bb994a72e1ce6abfe415d1978360b4780670
WinPear <leminhthang24012006@gmail.com>
2026-09-22 19:59:53 +0700

    fix: remove part 4

--- src/eshop-modular-monoliths/Modules/Catalog.Contracts/obj/Debug/net10.0/Catalog.Contracts.AssemblyInfo.cs
+++ src/eshop-modular-monoliths/Modules/Catalog.Contracts/obj/Debug/net10.0/Catalog.Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c2a19ccb185679161e1eb6ed7dbb31827b3cab1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca9885281759dfcd6b63f72ca669dc24ef9b4a94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
